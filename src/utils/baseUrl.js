05d497af579dbc42d7b22179607f5a393964cfb5
khanakitclass <khanakitclass@gmail.com>
2024-08-22 12:06:42 +0530

    xcx

--- src/utils/baseUrl.js
+++ src/utils/baseUrl.js
@@ -1,4 +1,5 @@
 export const  BASE_URL = 'http://localhost:8000/'
 
 // export const ECOMMERCE_URL = "http://localhost:8080/api/v1/"
-export const ECOMMERCE_URL = "https://fruitable-backend-one.vercel.app/api/v1/"
+export const ECOMMERCE_URL = "https://abfruitables.vercel.app/api/v1/"
+ 
